69ed00d5d45d5afe4bc980cc9ef68595e8141971
Yasif <muyayasif@gmail.com>
2023-10-02 23:37:31 +0700

    tugas 4 mobile yasif

---
+++ android/app/src/main/kotlin/com/tokokita/tokokita/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tokokita.tokokita
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tokokita/ui/login_page.dart';
+import 'package:tokokita/ui/produk_detail.dart';
+import 'package:tokokita/ui/produk_page.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  Future<bool> checkLoginStatus() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+    return isLoggedIn;
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Toko Kita',
+      debugShowCheckedModeBanner: false,
+      home: FutureBuilder<bool>(
+        future: checkLoginStatus(),
+        builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.done) {
+              return snapshot.data == true  ? ProdukPage() : LoginPage();
+            }else{
+              return CircularProgressIndicator();
+            }
+        },
+      ),
+    );
+  }
+}
